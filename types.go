61f504a8f27c7c4a5f108ebe3e7eb1aedf45e922
Aurelio Buonomo <48835871+auribuo@users.noreply.github.com>
2024-10-29 15:54:21 +0100

    Removed useless type info

--- types.go
+++ types.go
@@ -1,36 +1,10 @@
 package main
 
-import (
-	"os"
-	"time"
-)
-
 // Variable describes a variable that should be interpolated in the base url and the query parameters
 type Variable struct {
 	Name       string   `yaml:"name" json:"name" validate:"required"`         // Name is the name of the variable
 	IsConstant bool     `yaml:"constant" json:"constant" validate:"required"` // IsConstant is true if the value of the variable is constant or else false
 	Values     []string `yaml:"values" json:"values" validate:"required"`     // Values are all the possible values of the variable (only 1 in case of a constant)
-}
-
-func (v Variable) Value(index int) any {
-	if v.IsConstant {
-		return v.Values[0]
-	}
-	return v.Values[index]
-}
-
-// VariableMap is a map of variables that will be generated from a collection of
-// EndpointParameter and will be used when executing the go template
-type VariableMap map[string]any
-
-// Env is a function that returns the value of a given environment variable
-func (m VariableMap) Env(key string) string {
-	return os.Getenv(key)
-}
-
-// Now is a function that returns the current time in the given format (see Time.Format)
-func (m VariableMap) Now(format string) string {
-	return time.Now().Format(format)
 }
 
 type JwtLoginOptions struct {
@@ -48,9 +22,6 @@ type QueryDefinition struct {
 // Endpoint is the definition of an endpoint to test with all its query
 // parameters, variables and its result schema
 type Endpoint struct {
-	FileName           string            `yaml:"-" json:"-"`                                                                    // FileName is the name of the file that contains the definition
-	FullPath           string            `yaml:"-" json:"-"`                                                                    // FullPath is the full path of the file that contains the definition
-	Secrets            map[string]string `yaml:"-" json:"-"`                                                                    // Secrets are the secrets for this definition loaded from the secrets file
 	Name               string            `yaml:"name,omitempty" json:"name,omitempty" validate:"required"`                      // Name is the name of the endpoint
 	IsEnabled          bool              `yaml:"enabled,omitempty" json:"enabled,omitempty"`                                    // IsEnabled is a boolean that indicates if the endpoint is enabled (not contained in the definition)
 	BaseUrl            string            `yaml:"base_url,omitempty" json:"baseUrl,omitempty" validate:"required"`               // BaseUrl is the base path of the endpoint
